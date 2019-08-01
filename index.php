33a14bd3afe2c430112964b9a5e8cae69cb48a57
AyuBBlack <ayub.gadiev95@gmail.com>
2019-08-01 23:54:48 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,172 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Главная страница</title>
+
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<main>
+	<header id="top">
+		<div id="top-left">
+			<img src="images/logo.png" alt="Логотип">
+		</div>
+		<div id="top-right">
+			<div id="top-right-top">
+				<div id="top-right-common">
+					<div id="top-right-social">
+						<ul>
+							<li>
+								<a href="#">
+									<img src="images/social/vk.png" alt="Вконтакте">
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<img src="images/social/fb.png" alt="Facebook">
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<img src="images/social/tw.png" alt="Twitter">
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<img src="images/social/yt.png" alt="Youtube">
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<img src="images/social/ig.png" alt="Instagram">
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<img src="images/social/rss.png" alt="RSS">
+								</a>
+							</li>
+						</ul>
+					</div>
+					<div id="top-right-search">
+						<form action="" method="get">
+							<input type="text" name="" placeholder="Что ищем?"><input type="submit" name="" value="Поиск">
+						</form>
+					</div>
+				</div>
+			</div>
+			<div id="top-right-bottom">
+				<nav id="menu">
+					<ul>
+						<li>
+							<a href="#">Новости</a>
+						</li>
+						<li>
+							<a href="#">Аналитика</a>
+						</li>
+						<li>
+							<a href="#">Интервью</a>
+						</li>
+						<li>
+							<a href="#">Опросы</a>
+						</li>
+						<li>
+							<a href="#">Контакты</a>
+						</li>
+						<li>
+							<a href="#">Реклама</a>
+						</li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+	</header>
+	<section id="content">
+		<div id="content-left">
+			<div class="title">
+				<span>главные новости</span>
+			</div>
+
+			<div id="mainnews">
+				<div id="topnews">
+					<div id="topnews-bottom">
+						<a href="#">Нурди Нухажиев назвал пиар-акцию ЛГБТ сообщества – гнусной провокацией</a>
+						<div id="topnews-date">
+							СЕГОДНЯ В 13:57
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div id="content-right">
+			<div id="fresh">
+				<div class="title">
+					<span>свежее</span>
+				</div>
+				<div class="right-block-date">
+					<span>сегодня в 21:00</span>
+				</div>
+				<div class="right-block-text">
+					<a href="#">Власти китайского региона Синьцзян записывают в террористы за соблюдение Шариата</a> 
+				</div>
+				<div class="right-block-date">
+					<span>сегодня в 21:00</span>
+				</div>
+				<div class="right-block-text">
+					<a href="#">Рамзан Кадыров участвует в заседании правкомиссии Правительства РФ по проблемам развития регионов0</a> 
+				</div>
+				<div class="right-block-date">
+					<span>сегодня в 21:00</span>
+				</div>
+				<div class="right-block-text">
+					<a href="#">В Гудермесе прошла республиканская ярмарка вакансий рабочих и учебных мест</a> 
+				</div>
+				<div class="right-block-date">
+					<span>сегодня в 21:00</span>
+				</div>
+				<div class="right-block-text">
+					<a href="#">Даудов подвел промежуточные итоги месячника по санитарной очистке ЧР</a> 
+				</div>
+				<div class="right-block-date">
+					<span>сегодня в 21:00</span>
+				</div>
+				<div class="right-block-text">
+					<a href="#">Власти китайского региона Синьцзян записывают в террористы за соблюдение Шариата</a> 
+				</div>
+				<div class="right-block-date">
+					<span>сегодня в 21:00</span>
+				</div>
+				<div class="right-block-text">
+					<a href="#">Игроки «Терека» отметились в своих национальных командах</a> 
+				</div>
+				<div class="right-block-date">
+					<span>сегодня в 21:00</span>
+				</div>
+				<div class="right-block-text">
+					<a href="#">В ГГНТУ пройдет Съезд экологов России</a> 
+				</div>
+				<div class="right-block-date">
+					<span>сегодня в 21:00</span>
+				</div>
+				<div class="right-block-text">
+					<a href="#">Лема Дадаев и Апти Алаудинов обсудили вопросы правопорядка</a> 
+				</div>
+				<div class="right-block-date">
+					<span>сегодня в 21:00</span>
+				</div>
+				<div class="right-block-text">
+					<a href="#">Чеченские депутаты проводят встречи с молодежью</a> 
+				</div>
+				<div class="right-block-date">
+					<span>сегодня в 21:00</span>
+				</div>
+				<div class="right-block-text">
+					<a href="#">Совершен первый рейс Стамбул – Грозный - Стамбул</a> 
+				</div>
+
+			</div>
+		</div>
+	</section>
+</main>
+</body>
+</html>
